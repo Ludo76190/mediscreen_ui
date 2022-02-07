caf45bd298979549163a38074f0ed6d0682cc234
Ludovic Allegaert <ludovic.allegaert@axa.fr>
2022-02-07 12:06:21 +0100

    add swagger

--- src/main/java/com/ludo/msui/MsuiApplication.java
+++ src/main/java/com/ludo/msui/MsuiApplication.java
@@ -2,8 +2,10 @@ package com.ludo.msui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsuiApplication {
 
     public static void main(String[] args) {
