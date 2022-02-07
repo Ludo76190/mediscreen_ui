e6d7abed6698747ae166d258b8185f62056720fd
Ludovic Allegaert <ludovic.allegaert@axa.fr>
2022-02-07 11:23:30 +0100

    initial commit

---
+++ src/main/java/com/ludo/msui/MsuiApplication.java
@@ -0,0 +1,13 @@
+package com.ludo.msui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ludo/msui/MsuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludo.msui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
